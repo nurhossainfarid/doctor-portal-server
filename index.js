de428467203b7d3351c25a1ad7decf166a0144cf
nurhossainfarid <faahsan1516@gmail.com>
2022-05-14 19:31:11 +0600

    add basic mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -7,6 +8,30 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.ACCESS_USER}:${process.env.ACCESS_PASSWORD}@cluster0.ikcqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctor-portal').collection('services')
+
+        // find all data
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to doctor portal')
